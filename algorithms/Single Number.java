031d0a5a02ad455eb7f6ed23bd47968a5580d59e
Liu Cheng <lc504906985@gmail.com>
2015-07-28 09:02:11 +0800

    question 136
    xor

---
+++ algorithms/Single Number.java
@@ -0,0 +1,14 @@
+// Given an array of integers, every element appears twice except for one. Find that single one.
+
+// Note:
+// Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+
+public class Solution {
+    public int singleNumber(int[] nums) {
+		int answer = 0;
+        for (int i = 0; i < nums.length; i++) {
+			answer ^= nums[i];
+		}
+		return answer;
+    }
+}
