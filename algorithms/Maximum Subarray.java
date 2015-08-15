b1781c6082b22b4ad6ebe50689af58792ad7978b
Liu Cheng <lc504906985@gmail.com>
2015-08-15 08:55:29 +0800

    动态规划
    如果某次和是负数,那么就到此为止,重新开始计数

---
+++ algorithms/Maximum Subarray.java
@@ -0,0 +1,26 @@
+// Find the contiguous subarray within an array (containing at least one number) which has the largest sum.
+
+// For example, given the array [−2,1,−3,4,−1,2,1,−5,4],
+// the contiguous subarray [4,−1,2,1] has the largest sum = 6.
+
+// click to show more practice.
+
+// More practice:
+// If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
+
+public class Solution {
+	public int maxSubArray(int[] nums) {
+		int sum = 0;
+		int answer = Integer.MIN_VALUE;
+		
+		for (int i = 0; i < nums.length; i++) {
+			if (sum <= 0) {
+				sum = 0;
+			}
+			sum += nums[i];
+			answer = Math.max(sum, answer);
+		}
+		
+		return answer;
+	}
+}
