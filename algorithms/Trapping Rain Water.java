7988e99ca954abe6661f22cdfa2c7bb69ef89049
Liu Cheng <lc504906985@gmail.com>
2015-09-18 11:08:47 +0800

    从两边往中间搜

---
+++ algorithms/Trapping Rain Water.java
@@ -0,0 +1,30 @@
+// Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it is able to trap after raining.
+
+// For example, 
+// Given [0,1,0,2,1,0,1,3,2,1,2,1], return 6.
+
+
+// The above elevation map is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units of rain water (blue section) are being trapped. Thanks Marcos for contributing this image!
+
+public class Solution {
+	public int trap(int[] height) {
+		int left = 0;
+		int right = height.length - 1;
+		int leftMax = 0;
+		int rightMax = 0;
+		int space = 0;
+		
+		while (left < right) {
+			leftMax = Math.max(leftMax, height[left]);
+			rightMax = Math.max(rightMax, height[right]);
+			if (leftMax < rightMax) {
+				space += leftMax - height[left];
+				left++;
+			} else {
+				space += rightMax - height[right];
+				right--;
+			}
+		}
+		return space;
+	}
+}
