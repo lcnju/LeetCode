049208b45ac833fe3d413e045431449ffbe5dd41
Liu Cheng <lc504906985@gmail.com>
2016-01-16 13:23:35 +0800

    JOIN ON WHERE

--- database/Rising Temperature.sql
+++ database/Rising Temperature.sql
@@ -17,3 +17,22 @@
 -- +----+
 -- Subscribe to see which companies asked this question
 
+DROP DATABASE IF EXISTS leetcode;
+CREATE DATABASE leetcode;
+USE leetcode
+
+CREATE TABLE Weather (
+    Id INT,
+    Date DATE,
+    Temperature INT
+);
+
+INSERT INTO Weather VALUE('1', '2015-01-01', '10');
+INSERT INTO Weather VALUE('2', '2015-01-02', '20');
+INSERT INTO Weather VALUE('3', '2015-01-04', '25');
+INSERT INTO Weather VALUE('4', '2015-01-05', '30');
+
+# Write your MySQL query statement below
+SELECT w2.Id
+FROM Weather w1 JOIN Weather w2 ON TO_DAYS(w1.Date) - TO_DAYS(w2.Date) = -1
+WHERE w1.Temperature < w2.Temperature;
